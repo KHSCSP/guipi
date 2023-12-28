ebabad292f7ef6617c11552b5634e47ace332b4e
scneeman <30477959+scneeman@users.noreply.github.com>
2023-12-27 20:37:26 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+# this must be run in a pygame trinket
+
+import tkinter as tk
+
+# define any functions
+def get_pi():
+  f = open("pi_short.txt", "r")  # open the file for reading
+  pi = f.read() # read the data into one long string
+  f.close() # close the file to release memory
+  # TODOclean up
+  pi = pi.replace("\n", "")
+  pi = pi.replace(" ", "")
+  return pi
+
+
+
+
+# call the get_pi function
+pi = get_pi()
+
+
+# section 1 - setup main window
+
+
+
+# section 2 - create frame1 - label_pi, label_instr, entry_bday, btn_go, label_ans
+
+
+
+# 'raise' the frame you want visible first
+frame1.tkraise()
+root.mainloop()
+
+
