0c20556bcc725d971a6932a60b72869718b8e0d1
me_chromebook <heyscottn@gmail.com>
2023-12-28 09:17:43 -0600

    edits

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 # define any functions
 def get_pi():
-  f = open("guipi/pi_short.txt", "r")  # open the file for reading
+  f = open("u11guipi/pi_short.txt", "r")  # open the file for reading
   pi = f.read() # read the data into one long string
   f.close() # close the file to release memory
   # TODOclean up
